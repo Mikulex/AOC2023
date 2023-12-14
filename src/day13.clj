f86ebdd1424b72bafff3a869f660e64f9486c1e8
Mikulex <mikulex@mikulex.com>
2023-12-14 23:36:56 +0100

    solve 13/1

--- src/day13.clj
+++ src/day13.clj
@@ -5,32 +5,52 @@
 (def demo_input_file (str (System/getProperty "user.dir") "/inputs/day13/" "demo.txt"))
 (def real_input_file (str (System/getProperty "user.dir") "/inputs/day13/" "input.txt"))
 
+(defn columns
+  [lines]
+  (map str/join (partition (count lines)
+    (for [x (range (count (first lines))) y (range (count lines))] 
+    (get-in lines [y x])))))
+
+(defn reflection-point 
+  [block]
+  (->> block
+       (partition 2 1)
+       (keep #(apply = %))
+       (map-indexed #(vector %1 %2))
+       (filter second)
+       (map first)))
+
+(defn valid-mirror-point
+  [[points block]]
+  (filter (fn [p] 
+            (loop [i p
+                   j (inc p)]
+              (let [i-line (get block i)
+                    j-line (get block j)]
+                (cond
+                  (or (nil? i-line) (nil? j-line)) true
+                  (not= i-line j-line) false
+                  :else (recur (dec i) (inc j)))))) 
+          points))
+
+
+(defn mirror-vals [blocks]
+  (->> blocks
+       (map #(into [] %))
+       (map (juxt reflection-point identity))
+       (filter #(not-empty (first %)))
+       (map valid-mirror-point)
+       (filter not-empty)
+       (flatten)
+       (map inc)))
+
 (defn solve1 
   [file]
-  (let [lines (str/split (slurp file) #"\n\n")]
-    (->> lines 
-         (map #(str/split % #"\n"))
-         (map (fn [coll] (map-indexed #(vector %1 %2) coll)))
-         (map (fn [coll] (sort-by #(second %) coll)))
-         (map (fn [coll] (partition-by #(second %) coll)))
-         (map (fn [coll] (filter #(= 2 (count %)) coll))))))
-
-
-(sort-by #(second %) [[0 "333"] [1 "111"] [2 "333"]])
-
-(defn solve2
-  [file]
-  (let [lines (str/split (slurp file) #"\n")]
-    ))
-
+  (let [lines (str/split (slurp file) #"\n\n")
+        h (mapv #(str/split % #"\n") lines)
+        v (mapv columns h)
+        [h-res v-res] (map mirror-vals [h v])]
+    (reduce + (concat (map #(* 100 %) h-res) v-res))))
+    
 (solve1 demo_input_file)
-((([2 "##......#"] [3 "##......#"])
-  ([1 "..#.##.#."] [4 "..#.##.#."]))
-
- (([3 "#####.##."] [4 "#####.##."])
-  ([1 "#....#..#"] [6 "#....#..#"])
-  ([2 "..##..###"] [5 "..##..###"])))
 (solve1 real_input_file)
-
-(solve2 demo_input_file)
-(time (solve2 real_input_file))
