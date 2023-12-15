d1d063b52fcadcaa1835b4bfca7c91f822118d21
Mikulex <mikulex@mikulex.com>
2023-12-15 20:49:36 +0100

    solve day15/2

--- src/day13.clj
+++ src/day13.clj
@@ -11,35 +11,38 @@
     (for [x (range (count (first lines))) y (range (count lines))] 
     (get-in lines [y x])))))
 
+(defn reflection?
+  [[f s] error] (= error (count (filter false? (map = f s)))))
+
 (defn reflection-point 
-  [block]
+  [block error]
   (->> block
        (partition 2 1)
-       (keep #(apply = %))
+       (keep #(reflection? % error))
        (map-indexed #(vector %1 %2))
        (filter second)
        (map first)))
 
 (defn valid-mirror-point
-  [[points block]]
+  [[points block] error]
   (filter (fn [p] 
-            (loop [i p
-                   j (inc p)]
+            (loop [i (dec p)
+                   j (inc (inc p))]
               (let [i-line (get block i)
                     j-line (get block j)]
                 (cond
                   (or (nil? i-line) (nil? j-line)) true
-                  (not= i-line j-line) false
+                  (not (reflection? [i-line j-line] error)) false
                   :else (recur (dec i) (inc j)))))) 
           points))
 
 
-(defn mirror-vals [blocks]
+(defn mirror-vals [blocks error]
   (->> blocks
        (map #(into [] %))
-       (map (juxt reflection-point identity))
+       (map (juxt #(reflection-point % error) identity))
        (filter #(not-empty (first %)))
-       (map valid-mirror-point)
+       (map #(valid-mirror-point % error))
        (filter not-empty)
        (flatten)
        (map inc)))
@@ -49,8 +52,20 @@
   (let [lines (str/split (slurp file) #"\n\n")
         h (mapv #(str/split % #"\n") lines)
         v (mapv columns h)
-        [h-res v-res] (map mirror-vals [h v])]
+        [h-res v-res] (map #(mirror-vals % 0) [h v])]
     (reduce + (concat (map #(* 100 %) h-res) v-res))))
-    
+
+(defn solve2
+  [file]
+  (let [lines (str/split (slurp file) #"\n\n")
+        h (mapv #(str/split % #"\n") lines)
+        v (mapv columns h)
+        [h-res v-res] (map #(mirror-vals % 1) [h v])]
+    [h-res v-res]))
+
+
 (solve1 demo_input_file)
+(solve1 real_input_file)
+
+(solve2 demo_input_file)
 (solve1 real_input_file)
--- src/day14.clj
+++ src/day14.clj
@@ -10,23 +10,57 @@
     (for [x (range (count (first lines))) y (range (count lines))] 
     (get-in lines [y x])))))
 
-(defn move-rocks [lines]
-  (->> lines
+(def move-rocks (memoize (fn [lines orientation direction]
+  (->> (orientation lines)
          (map #(partition-by (partial = \#) %))
          (map (fn [portion] (map #(sort %) portion)))
-         (map (fn [portion] (map #(reverse %) portion)))
+         (map (fn [portion] (map #(direction %) portion)))
          (map #(apply concat %))
          (map #(apply str %))
-         (vec)))
+         (vec)
+         (orientation)))))
+
+(defn north [lines] (move-rocks lines transpose reverse))
+(defn south [lines] (move-rocks lines transpose identity))
+(defn west [lines] (move-rocks lines identity reverse))
+(defn east [lines] (move-rocks lines identity identity))
+
+(def spin-cycle (memoize (fn [l n]
+  (loop [lines l
+         iters n
+         last-iter []]
+    (cond 
+      (zero? iters) lines
+      (= last-iter lines) lines
+      :else (recur (east (south (west (north lines)))) (dec iters) lines))))))
+
+(defn calc-load [lines]
+  (reduce + (map-indexed (fn [idx freqs] 
+                           (* (- (count lines) idx) (or (get freqs \O) 0)))
+                         (map frequencies lines))))
 
 (defn solve1 
   [file]
-  (let [lines (transpose (str/split (slurp file) #"\n"))]
-    (reduce + (map-indexed (fn [idx freqs] (* (- (count lines) idx) 
-                                    (or (get freqs \O) 0)))
-                 (map frequencies (transpose (move-rocks lines)))))))
+  (let [lines (str/split (slurp file) #"\n")]
+    (calc-load (north lines))))
+
+(defn loop-points [lines]
+  (loop [idx 0
+         calculated {}]
+    (let [nth-cycle (spin-cycle lines idx)]
+      (if (contains? calculated nth-cycle) [(get calculated nth-cycle) (- idx (get calculated nth-cycle))]
+        (recur (inc idx) (assoc calculated nth-cycle idx))))))
+
+(defn solve2
+  [file]
+  (let [lines (str/split (slurp file) #"\n")
+        iters 1000000000
+        [idx len] (loop-points lines)]
+    (prn idx len)
+    (calc-load (spin-cycle lines (+ idx (mod (- iters idx) len))))))
+
 (solve1 demo_input_file)
 (solve1 real_input_file)
 
-(solve2 demo_input_file)
-(solve1 real_input_file)
+(time (solve2 demo_input_file))
+(time (solve2 real_input_file))
