823b83126f797607a73264dbee8200968e23119e
Mikulex <mikulex@mikulex.com>
2023-12-17 13:04:05 +0100

    solve day16/2

--- src/day16.clj
+++ src/day16.clj
@@ -27,34 +27,48 @@
     :else (throw (Exception. "unmatched case while splitting"))))
 
 (defn energized-tiles [lines]
-  (letfn [(iter [cur facing visited]
-            (let [cur-tile (get-in lines cur)
-                  offset (facing direction)
-                  next-coord-same-dir (map + cur offset)]
-              (cond
-                (nil? cur-tile) visited
-                (contains? visited [cur facing]) visited
-                (= cur-tile \.) (recur next-coord-same-dir facing (conj visited [cur facing]))
-                (some #(= cur-tile %) [\- \|]) (if (splits? cur-tile facing)
-                                                 (apply set/union (map #(iter (map + cur (% direction)) % (conj visited [cur facing]))
-                                                                      (vals (facing turns))))
-                                                 (recur next-coord-same-dir facing (conj visited [cur facing])))
-                (some #(= cur-tile %) [\\ \/]) (recur (map + cur ((reflect cur-tile facing) direction)) (reflect cur-tile facing) (conj visited [cur facing]))
-                :else (throw (Exception. "unmatched case while traversing")))))]
-    (iter [0 0] :east #{})))
+  (fn iter [cur facing visited]
+    (let [cur-tile (get-in lines cur)
+          offset (facing direction)
+          next-coord-same-dir (map + cur offset)]
+      (cond
+        (nil? cur-tile) visited
+        (contains? visited [cur facing]) visited
+        (= cur-tile \.) (recur next-coord-same-dir facing (conj visited [cur facing]))
+        (some #(= cur-tile %) [\- \|]) (if (splits? cur-tile facing)
+                                         (apply set/union (map #(iter (map + cur (% direction)) % (conj visited [cur facing]))
+                                                               (vals (facing turns))))
+                                         (recur next-coord-same-dir facing (conj visited [cur facing])))
+        (some #(= cur-tile %) [\\ \/]) (recur (map + cur ((reflect cur-tile facing) direction)) (reflect cur-tile facing) (conj visited [cur facing]))
+        :else (throw (Exception. "unmatched case while traversing"))))))
 
+
+(defn top-left-path [lines]
+  ((energized-tiles lines) [0 0] :east #{}))
+
+(defn start-ranges [len]
+  {:east (for [a (range len) b (range len) :when (zero? b)] [a b])
+   :west (for [a (range len) b (range len) :when (= (dec len) b)] [a b])
+   :north (for [a (range len) b (range len) :when (= (dec len) a)] [a b])
+   :south (for [a (range len) b (range len) :when (zero? a)] [a b])})
+
+(defn all-paths [lines]
+  (let [len (count lines)]
+    (pmap (fn [[dir coord]]
+              (count (into #{} (map first ((energized-tiles lines) coord dir #{})))))
+         (mapcat (fn [[k v]] (map #(vector k %) v)) (start-ranges len)))))
 
 (defn solve1 
   [file]
   (let [lines (str/split (slurp file) #"\n")]
-    (count (into #{} (map first (energized-tiles lines))))))
+    (count (into #{} (map first (top-left-path lines))))))
 
 (defn solve2
   [file]
   (let [lines (str/split (slurp file) #"\n")]
-    ))
+  (apply max (all-paths lines))))
 
 (solve1 demo_input_file)
 (solve1 real_input_file)
-;(solve2 demo_input_file)
-;(solve2 real_input_file)
+(solve2 demo_input_file)
+(time (solve2 real_input_file))
